ae0e52c28b870a3213a2d7c476ceef6b8093bf67
Das <jyoti-sankar.das@capgemini.com>
2025-07-14 16:32:05 +0530

    update in API gateway

--- Capgemini/src/main/java/com/example/demo/model/CompanyInputDTO.java
+++ Capgemini/src/main/java/com/example/demo/model/CompanyInputDTO.java
@@ -1,5 +1,8 @@
 package com.example.demo.model;
 
+import lombok.Data;
+
+@Data
 public class CompanyInputDTO {
 	
 	private Long cmpid;
@@ -7,24 +10,24 @@ public class CompanyInputDTO {
 	private String address;
 	
 	
-	public Long getCmpid() {
-		return cmpid;
-	}
-	public void setCmpid(Long cmpid) {
-		this.cmpid = cmpid;
-	}
-	public String getCmpnmae() {
-		return cmpnmae;
-	}
-	public void setCmpnmae(String cmpnmae) {
-		this.cmpnmae = cmpnmae;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+//	public Long getCmpid() {
+//		return cmpid;
+//	}
+//	public void setCmpid(Long cmpid) {
+//		this.cmpid = cmpid;
+//	}
+//	public String getCmpnmae() {
+//		return cmpnmae;
+//	}
+//	public void setCmpnmae(String cmpnmae) {
+//		this.cmpnmae = cmpnmae;
+//	}
+//	public String getAddress() {
+//		return address;
+//	}
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}
 	
 
 }
